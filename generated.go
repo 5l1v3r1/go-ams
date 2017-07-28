db965c915754eac3b9217ec22da3dcea040de2ad
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-07-28 19:04:53 +0900

    feat: update generate file

--- generated.go
+++ generated.go
@@ -4,9 +4,10 @@ import (
 	"context"
 	"net/url"
 	"os"
+	"time"
 )
 
-func (c *Client) CreateAccessPolicy(name, durationInMinutes float64, permissions int) (*AccessPolicy, error) {
+func (c *Client) CreateAccessPolicy(name string, durationInMinutes float64, permissions int) (*AccessPolicy, error) {
 	return c.CreateAccessPolicyWithContext(context.Background(), name, durationInMinutes, permissions)
 }
 func (c *Client) DeleteAccessPolicy(accessPolicy *AccessPolicy) error {
@@ -20,6 +21,9 @@ func (c *Client) GetAssets() ([]Asset, error) {
 }
 func (c *Client) CreateAsset(name string) (*Asset, error) {
 	return c.CreateAssetWithContext(context.Background(), name)
+}
+func (c *Client) GetAssetFiles(asset *Asset) ([]AssetFile, error) {
+	return c.GetAssetFilesWithContext(context.Background(), asset)
 }
 func (c *Client) CreateAssetFile(assetID, name, mimeType string) (*AssetFile, error) {
 	return c.CreateAssetFileWithContext(context.Background(), assetID, name, mimeType)
@@ -36,13 +40,16 @@ func (c *Client) PutBlockList(uploadURL *url.URL, blockList []int) error {
 func (c *Client) EncodeAsset(mediaProcessorID, configuration string, asset *Asset) (*Job, error) {
 	return c.EncodeAssetWithContext(context.Background(), mediaProcessorID, configuration, asset)
 }
+func (c *Client) GetOutputMediaAssets(job *Job) ([]Asset, error) {
+	return c.GetOutputMediaAssetsWithContext(context.Background(), job)
+}
 func (c *Client) GetJob(jobID string) (*Job, error) {
 	return c.GetJobWithContext(context.Background(), jobID)
 }
 func (c *Client) WaitJob(job *Job) error {
 	return c.WaitJobWithContext(context.Background(), job)
 }
-func (c *Client) CreateLocator(accessPolicyID, assetID, startTime string, locatorType int) (*Locator, error) {
+func (c *Client) CreateLocator(accessPolicyID, assetID string, startTime time.Time, locatorType int) (*Locator, error) {
 	return c.CreateLocatorWithContext(context.Background(), accessPolicyID, assetID, startTime, locatorType)
 }
 func (c *Client) DeleteLocator(locator *Locator) error {
