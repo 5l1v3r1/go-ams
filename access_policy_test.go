43ee0a33ccdf9cca54976b302cc7751e43e7023f
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-08-04 14:46:02 +0900

    test: add simple test

--- access_policy_test.go
+++ access_policy_test.go
@@ -1,15 +1,18 @@
 package ams
 
 import (
+	"context"
+	"encoding/json"
 	"net/http"
+	"net/http/httptest"
 	"strings"
 	"testing"
-
-	"context"
-	"net/http/httptest"
+	"time"
 
 	"github.com/pkg/errors"
 )
+
+type Matcher func(string, string) bool
 
 type HeaderChecker struct {
 	h   http.Header
@@ -20,34 +23,24 @@ func NewHeaderChecker(h http.Header) *HeaderChecker {
 	return &HeaderChecker{h: h, err: nil}
 }
 
-func (hc *HeaderChecker) Assert(key, expected string) {
+func (hc *HeaderChecker) Match(key, expected string, matcher Matcher) {
 	if hc.err != nil {
 		return
 	}
-	hc.err = assertHeader(hc.h, key, expected)
+	hc.err = assertHeader(hc.h, key, expected, matcher)
 }
 
-func (hc *HeaderChecker) AssertPrefix(key, expected string) {
-	if hc.err != nil {
-		return
-	}
-	hc.err = assertHeaderPrefix(hc.h, key, expected)
+func (hc *HeaderChecker) Assert(key, expected string) {
+	hc.Match(key, expected, func(a, b string) bool { return a == b })
 }
 
 func (hc *HeaderChecker) Err() error {
 	return hc.err
 }
 
-func assertHeader(h http.Header, key, expected string) error {
-	if actual := h.Get(key); actual != expected {
+func assertHeader(h http.Header, key, expected string, matcher func(string, string) bool) error {
+	if actual := h.Get(key); !matcher(actual, expected) {
 		return errors.Errorf("%s: expected %#v, actual %#v", key, expected, actual)
-	}
-	return nil
-}
-
-func assertHeaderPrefix(h http.Header, key, expected string) error {
-	if actual := h.Get(key); !strings.HasPrefix(actual, expected) {
-		return errors.Errorf("%s: expected %#v*, actual %#v", key, expected, actual)
 	}
 	return nil
 }
@@ -64,7 +57,7 @@ func validateAMSHeader(verbose bool, h http.Header) error {
 		hc.Assert("Content-Type", "application/json")
 		hc.Assert("Accept", "application/json")
 	}
-	hc.AssertPrefix("Authorization", "Bearer ")
+	hc.Match("Authorization", "Bearer ", strings.HasPrefix)
 	return hc.Err()
 }
 
@@ -77,13 +70,45 @@ func TestClient_CreateAccessPolicy(t *testing.T) {
 		if err := validateAMSHeader(false, r.Header); err != nil {
 			t.Fatal(err)
 		}
+		var params struct {
+			Name              string
+			DurationInMinutes float64
+			Permissions       int
+
+			ID           string `json:"Id"`
+			Created      string
+			LastModified string
+			Metadata     string `json:"odata.metadata"`
+		}
+		if err := json.NewDecoder(r.Body).Decode(&params); err != nil {
+			t.Fatal(err)
+		}
+		if len(params.Name) == 0 {
+			t.Errorf("Name is required")
+		}
+		if params.DurationInMinutes <= 0 {
+			t.Errorf("DurationInMinutes must be greater than 0")
+		}
+		if params.Permissions < 0 || 15 < params.Permissions {
+			t.Errorf("invalid Permissions")
+		}
+		params.Metadata = "https://dummy.url"
+		params.ID = "nb:pid:UUID:sample-id"
+		params.Created = time.Now().UTC().Format(time.RFC3339)
+		params.LastModified = time.Now().UTC().Format(time.RFC3339)
+
+		resp, err := json.Marshal(params)
+		if err != nil {
+			t.Fatal(err)
+		}
 		w.WriteHeader(http.StatusCreated)
-		
+		w.Write(resp)
 	})
+
 	s := httptest.NewServer(m)
 	defer s.Close()
 
-	client, err := NewClient(s.URL, dummyTokenSource)
+	client, err := NewClient(s.URL, newDummyTokenSource())
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -92,10 +117,35 @@ func TestClient_CreateAccessPolicy(t *testing.T) {
 		t.Fatal(err)
 	}
 	if ap.Name != "test" {
-		t.Fatal("name must be 'test'")
+		t.Errorf("response name unexpected")
+	}
+	if ap.Permissions != PermissionRead {
+		t.Errorf("response permissions unexpected")
+	}
+	if ap.DurationInMinutes != 400 {
+		t.Errorf("response durationInMinutes unexpected")
 	}
 }
 
 func TestClient_DeleteAccessPolicy(t *testing.T) {
+	m := http.NewServeMux()
+	m.HandleFunc("/AccessPolicies('nb:pid:UUID:sample-id')", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			t.Fatalf("method must be DELETE, actual %s", r.Method)
+		}
+		if err := validateAMSHeader(false, r.Header); err != nil {
+			t.Fatal(err)
+		}
+		w.WriteHeader(http.StatusNoContent)
+	})
+	s := httptest.NewServer(m)
+	defer s.Close()
 
+	client, err := NewClient(s.URL, newDummyTokenSource())
+	if err != nil {
+		t.Fatal(err)
+	}
+	if err := client.DeleteAccessPolicy(context.TODO(), "nb:pid:UUID:sample-id"); err != nil {
+		t.Error(err)
+	}
 }
--- client_test.go
+++ client_test.go
@@ -6,40 +6,45 @@ import (
 	"golang.org/x/oauth2"
 )
 
-var (
-	dummyTokenSource = oauth2.StaticTokenSource(&oauth2.Token{
+func TestNewClient(t *testing.T) {
+	dummyTokenSource := newDummyTokenSource()
+	dummyURL := "http://example.ams.net/"
+
+	t.Run("withInvalidURL", func(t *testing.T) {
+		client, err := NewClient("", dummyTokenSource)
+		if err == nil {
+			t.Errorf("accept invalid url")
+		}
+		if client != nil {
+			t.Errorf("return invalid client")
+		}
+	})
+
+	t.Run("withInvalidTokenSource", func(t *testing.T) {
+		client, err := NewClient(dummyURL, nil)
+		if err == nil {
+			t.Errorf("accept invalid token source")
+		}
+		if client != nil {
+			t.Errorf("return invalid client")
+		}
+	})
+
+	t.Run("positive testing", func(t *testing.T) {
+		client, err := NewClient(dummyURL, dummyTokenSource)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if client == nil {
+			t.Errorf("return invalid client")
+		}
+	})
+}
+
+func newDummyTokenSource() oauth2.TokenSource {
+	dummyTokenSource := oauth2.StaticTokenSource(&oauth2.Token{
 		AccessToken: "<<dummy access token>>",
 		TokenType:   "Bearer",
 	})
-	dummyURL = "http://example.ams.net/"
-)
-
-func TestNewClientWithInvalidURL(t *testing.T) {
-	client, err := NewClient("", dummyTokenSource)
-	if err == nil {
-		t.Errorf("accept invalid url")
-	}
-	if client != nil {
-		t.Errorf("return invalid client")
-	}
-}
-
-func TestNewClientWithInvalidTokenSource(t *testing.T) {
-	client, err := NewClient(dummyURL, nil)
-	if err == nil {
-		t.Errorf("accept invalid token source")
-	}
-	if client != nil {
-		t.Errorf("return invalid client")
-	}
-}
-
-func TestNewClient(t *testing.T) {
-	client, err := NewClient(dummyURL, dummyTokenSource)
-	if err != nil {
-		t.Fatal(err)
-	}
-	if client == nil {
-		t.Errorf("return invalid client")
-	}
+	return dummyTokenSource
 }
