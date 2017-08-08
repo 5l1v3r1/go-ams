d280a14ea7dfc2025c1b89b26fac5010f3b449f6
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-08-08 13:45:39 +0900

    refactor: refactor GetMediaProcessors test

--- media_processor_test.go
+++ media_processor_test.go
@@ -1,7 +1,6 @@
 package ams
 
 import (
-	"encoding/json"
 	"net/http"
 	"testing"
 
@@ -12,51 +11,56 @@ import (
 )
 
 func TestClient_GetMediaProcessors(t *testing.T) {
-	expected := MediaProcessor{
-		ID:          "sample-id",
-		Name:        "Sample Media Processor",
-		Description: "Sample Media Processor, for test",
-		SKU:         "Stock Keeping Unit?",
-		Vendor:      "Sample Vendor",
-		Version:     "1.0.0",
+	expected := []MediaProcessor{
+		{
+			ID:          "sample-id1",
+			Name:        "Sample Media Processor",
+			Description: "Sample Media Processor, for test",
+			SKU:         "Stock Keeping Unit?",
+			Vendor:      "Sample Vendor",
+			Version:     "1.0.0",
+		},
+		{
+			ID:          "sample-id2",
+			Name:        "Standard Media Processor",
+			Description: "Standard Media Processor, for test",
+			SKU:         "Stock Keeping Unit??",
+			Vendor:      "Sample Inc.",
+			Version:     "1.1.0",
+		},
+		{
+			ID:          "sample-id3",
+			Name:        "Simple Media Processor",
+			Description: "Simple Media Processor, for test",
+			SKU:         "Stock Keeping Unit???",
+			Vendor:      "Simple Vendor",
+			Version:     "2.0.0",
+		},
+	}
+	resp := struct {
+		Value []MediaProcessor `json:"value"`
+	}{
+		Value: expected,
 	}
 	m := http.NewServeMux()
-	m.HandleFunc("/MediaProcessors", func(w http.ResponseWriter, r *http.Request) {
-		testRequestMethod(t, r, http.MethodGet)
-		testAMSHeader(t, r.Header, false)
+	m.HandleFunc("/MediaProcessors",
+		testJSONHandler(t, http.MethodGet, false, http.StatusOK, resp),
+	)
 
-		resp := struct {
-			Value []MediaProcessor `json:"value"`
-		}{
-			Value: []MediaProcessor{
-				expected,
-				expected,
-			},
-		}
-		w.WriteHeader(http.StatusOK)
-		if err := json.NewEncoder(w).Encode(resp); err != nil {
-			t.Fatal(err)
-		}
-	})
+	s := httptest.NewServer(m)
+	defer s.Close()
 
-	ts := httptest.NewServer(m)
-	defer ts.Close()
-
-	client, err := NewClient(ts.URL, testTokenSource())
+	client, err := NewClient(s.URL, testTokenSource())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	mediaProcessors, err := client.GetMediaProcessors(context.TODO())
+	actual, err := client.GetMediaProcessors(context.TODO())
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	if !reflect.DeepEqual(expected, mediaProcessors[0]) {
-		t.Errorf("unexpected result[0]. expected: %#v, actual: %#v", expected, mediaProcessors[0])
-	}
-
-	if !reflect.DeepEqual(expected, mediaProcessors[1]) {
-		t.Errorf("unexpected result[1]. expected: %#v, actual: %#v", expected, mediaProcessors[1])
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("unexpected media processors. expected: %#v, actual: %#v", expected, actual)
 	}
 }
