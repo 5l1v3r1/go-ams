87ed90a4867aebd525f54b7e63072d35cc1bc0f2
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-08-08 14:00:35 +0900

    test: add GetAssets test

--- asset_test.go
+++ asset_test.go
@@ -32,3 +32,31 @@ func TestClient_GetAsset(t *testing.T) {
 		t.Errorf("unexpected asset. expected: %#v, actual: %#v", expected, actual)
 	}
 }
+
+func TestClient_GetAssets(t *testing.T) {
+	expected := []Asset{
+		testAsset("asset1", "Sample 1"),
+		testAsset("asset2", "Sample 2"),
+		testAsset("asset4", "Example 1"),
+	}
+	m := http.NewServeMux()
+	m.HandleFunc("/Assets",
+		testJSONHandler(t, http.MethodGet, false, http.StatusOK, testWrapValue(expected)),
+	)
+
+	s := httptest.NewServer(m)
+	defer s.Close()
+
+	client, err := NewClient(s.URL, testTokenSource())
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	actual, err := client.GetAssets(context.TODO())
+	if err != nil {
+		t.Error(err)
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("unexpected assets. expected: %#v, actual: %#v", expected, actual)
+	}
+}
