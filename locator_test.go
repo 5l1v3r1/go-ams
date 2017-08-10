af8b52f496c639e603e71e934b747c1e7414d5f8
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-08-10 13:30:57 +0900

    test: add DeleteLocator test

--- locator_test.go
+++ locator_test.go
@@ -3,6 +3,7 @@ package ams
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"reflect"
@@ -73,5 +74,19 @@ func TestClient_CreateLocator(t *testing.T) {
 	}
 	if !reflect.DeepEqual(actual, expected) {
 		t.Errorf("unexpected locator. expected: %#v, actual: %#v", expected, actual)
+	}
+}
+
+func TestClient_DeleteLocator(t *testing.T) {
+	locatorID := "delete-locator-id"
+
+	m := http.NewServeMux()
+	m.HandleFunc(fmt.Sprintf("/Locators('%v')", locatorID),
+		testJSONHandler(t, http.MethodDelete, false, http.StatusNoContent, nil),
+	)
+	s := httptest.NewServer(m)
+	client := testClient(t, s.URL)
+	if err := client.DeleteLocator(context.TODO(), locatorID); err != nil {
+		t.Error(err)
 	}
 }
