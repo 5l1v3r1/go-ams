902fcd8a43f4febf2034a423f1d08a46ee5f1e96
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-10-30 19:12:15 +0900

    chore: change description

--- testing_request.go
+++ testing_request.go
@@ -7,6 +7,6 @@ import (
 
 func testRequestMethod(t *testing.T, r *http.Request, expected string) {
 	if r.Method != expected {
-		t.Fatalf("method must be %v, actual %v", expected, r.Method)
+		t.Fatalf("unexpected method. expected: %v, but got: %v", expected, r.Method)
 	}
 }
