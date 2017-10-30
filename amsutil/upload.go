0757fe2b782f2cd619eb573aaabe85fa2dd4ab1e
Nao YONASHIRO <yonashiro@r.recruit.co.jp>
2017-10-30 18:33:50 +0900

    refactor: use SASClient.Upload

--- amsutil/upload.go
+++ amsutil/upload.go
@@ -1,13 +1,12 @@
 package amsutil
 
 import (
-	"bytes"
 	"context"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"mime"
 	"os"
-	"path"
+	"path/filepath"
 	"strings"
 	"time"
 
@@ -20,168 +19,126 @@ const (
 	uploadDurationInMinute = 440.0
 )
 
-type multiError struct {
-	errs []error
-}
-
-func (m *multiError) Error() string {
-	messages := make([]string, 0, len(m.errs))
-	for _, err := range m.errs {
-		messages = append(messages, err.Error())
-	}
-	return fmt.Sprintf("multiple error: %s", strings.Join(messages, ","))
-}
+var TimeNow func() time.Time = time.Now
 
 type Uploadable interface {
+	io.Reader
 	Name() string
-	Size() int64
-	Blobs() []ams.Blob
 }
 
 type uploadable struct {
-	name  string
-	size  int64
-	blobs []ams.Blob
+	r    io.Reader
+	name string
 }
 
-func (u *uploadable) Name() string      { return u.name }
-func (u *uploadable) Size() int64       { return u.size }
-func (u *uploadable) Blobs() []ams.Blob { return u.blobs }
-
-func SplitBlob(blob ams.Blob, chunkSize int64) ([]ams.Blob, error) {
-	size := blob.Size()
-	body, err := ioutil.ReadAll(blob)
-	if err != nil {
-		return nil, errors.Wrap(err, "blob failed to read")
-	}
-	var chunk []byte
-	chunks := make([]ams.Blob, 0, (size+chunkSize-1)/chunkSize)
-	for int64(len(body)) >= chunkSize {
-		chunk, body = body[:chunkSize], body[chunkSize:]
-		chunks = append(chunks, bytes.NewReader(chunk))
-	}
-	if len(body) > 0 {
-		chunks = append(chunks, bytes.NewReader(body))
-	}
-	return chunks, nil
+func (u *uploadable) Read(p []byte) (n int, err error) {
+	return u.r.Read(p)
 }
 
-func NewUploadable(name string, blob ams.Blob, chunkSize int64) (Uploadable, error) {
-	blobs, err := SplitBlob(blob, chunkSize)
-	if err != nil {
-		return nil, errors.Wrap(err, "blob failed to split")
+func (u *uploadable) Name() string {
+	return u.name
+}
+
+func NewUploadableFile(file *os.File) (Uploadable, error) {
+	if file == nil {
+		return nil, errors.New("missing file")
 	}
 	return &uploadable{
-		name:  name,
-		size:  blob.Size(),
-		blobs: blobs,
+		r:    file,
+		name: filepath.Base(file.Name()),
 	}, nil
 }
 
-func UploadFile(ctx context.Context, client *ams.Client, file *os.File, chunkSize, worker int64) (*ams.Asset, error) {
+func UploadFile(ctx context.Context, client *ams.Client, file *os.File, chunkSize int64, workers uint) (*ams.Asset, error) {
+	if ctx == nil {
+		return nil, errors.New("missing ctx")
+	}
 	if client == nil {
-		return nil, errors.New("client missing")
+		return nil, errors.New("missing client")
 	}
 	if file == nil {
-		return nil, errors.New("file missing")
+		return nil, errors.New("missing file")
+	}
+	if chunkSize <= 0 {
+		return nil, errors.New("chunkSize must be greater than 0")
+	}
+	if workers == 0 {
+		return nil, errors.New("workers must be greater than 0")
 	}
 
-	fblob, err := ams.NewFileBlob(file)
-	if err != nil {
-		return nil, errors.Wrap(err, "file blob construct failed")
-	}
-
-	mimeType := mime.TypeByExtension(path.Ext(fblob.Name()))
+	u, _ := NewUploadableFile(file)
+	mimeType := mime.TypeByExtension(filepath.Ext(u.Name()))
 	if !strings.HasPrefix(mimeType, "video/") {
-		return nil, errors.Errorf("invalid file type. expected video/*, actual '%v'", mimeType)
+		return nil, errors.Errorf("invalid file type. expected video/*, but got '%v'", mimeType)
 	}
 
-	uploadable, err := NewUploadable(fblob.Name(), fblob, chunkSize)
-	if err != nil {
-		return nil, errors.Wrap(err, "uploadable failed to construct")
-	}
-
-	return Upload(ctx, client, uploadable, mimeType, worker)
+	return Upload(ctx, client, u, mimeType, chunkSize, workers)
 }
 
-func Upload(ctx context.Context, client *ams.Client, uploadable Uploadable, mimeType string, worker int64) (*ams.Asset, error) {
+func Upload(ctx context.Context, client *ams.Client, uploadable Uploadable, mimeType string, chunkSize int64, workers uint) (*ams.Asset, error) {
+	if ctx == nil {
+		return nil, errors.New("missing ctx")
+	}
 	if client == nil {
-		return nil, errors.New("client missing")
+		return nil, errors.New("missing client")
 	}
 	if uploadable == nil {
-		return nil, errors.New("uploadable missing")
+		return nil, errors.New("missing uploadable")
+	}
+	if !strings.HasPrefix(mimeType, "video/") {
+		return nil, errors.Errorf("invalid mime type. expected video/*, but got '%v'", mimeType)
+	}
+	if chunkSize <= 0 {
+		return nil, errors.New("chunkSize must be greater than 0")
+	}
+	if workers == 0 {
+		return nil, errors.New("workers must be greater than 0")
 	}
 
 	name := uploadable.Name()
 	asset, err := client.CreateAsset(ctx, name)
 	if err != nil {
-		return nil, errors.Wrapf(err, "create asset failed. name='%s'", name)
+		return nil, errors.Wrapf(err, "failed to create asset. name='%s'", name)
 	}
 
 	assetFile, err := client.CreateAssetFile(ctx, asset.ID, name, mimeType)
 	if err != nil {
-		return nil, errors.Wrapf(err, "create asset file failed. assetID='%s'", asset.ID)
+		return nil, errors.Wrapf(err, "failed to create asset file. assetID='%s'", asset.ID)
 	}
 
 	accessPolicy, err := client.CreateAccessPolicy(ctx, uploadPolicyName, uploadDurationInMinute, ams.PermissionWrite)
 	if err != nil {
-		return nil, errors.Wrap(err, "create access policy failed")
+		return nil, errors.Wrap(err, "failed to create access policy")
 	}
 	defer client.DeleteAccessPolicy(ctx, accessPolicy.ID)
 
-	startTime := time.Now().Add(-5 * time.Minute)
+	// ref: https://docs.microsoft.com/en-US/azure/media-services/media-services-rest-upload-files
+	// for clock skew
+	startTime := TimeNow().Add(-5 * time.Minute)
 	locator, err := client.CreateLocator(ctx, accessPolicy.ID, asset.ID, startTime, ams.LocatorSAS)
 	if err != nil {
-		return nil, errors.Wrap(err, "create locator failed")
+		return nil, errors.Wrap(err, "failed to create locator")
 	}
 	defer client.DeleteLocator(ctx, locator.ID)
 
 	uploadURL, err := locator.ToUploadURL(name)
 	if err != nil {
-		return nil, errors.Wrapf(err, "upload url build failed")
+		return nil, errors.Wrap(err, "failed to construct upload url")
 	}
 
-	blobs := uploadable.Blobs()
-	blobsSize := len(blobs)
-	jobs := make(chan int, worker)
-	errs := make(chan error, worker)
-
-	for w := int64(0); w < worker; w++ {
-		go func(c context.Context, id int64, in <-chan int, e chan<- error) {
-			for i := range in {
-				blockID := fmt.Sprintf("block-id-%v", i+1)
-				e <- client.PutBlob(c, uploadURL, blobs[i], blockID)
-			}
-		}(ctx, w, jobs, errs)
+	sasc, err := client.NewSASClient(uploadURL.String())
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to construct SASClient")
 	}
 
-	var blockList []string
-	for i := 0; i < blobsSize; i++ {
-		blockID := fmt.Sprintf("block-id-%v", i+1)
-		blockList = append(blockList, blockID)
-		jobs <- i
-	}
-	close(jobs)
-
-	var es []error
-	for i := 0; i < blobsSize; i++ {
-		err := <-errs
-		if err != nil {
-			es = append(es, err)
-		}
+	contentLength, err := sasc.Upload(ctx, uploadable, chunkSize, workers)
+	if err != nil {
+		return nil, err
 	}
 
-	if len(es) != 0 {
-		return nil, &multiError{es}
-	}
-
-	if err := client.PutBlockList(ctx, uploadURL, blockList); err != nil {
-		return nil, errors.Wrap(err, "put block list failed")
-	}
-
-	assetFile.ContentFileSize = fmt.Sprint(uploadable.Size())
+	assetFile.ContentFileSize = fmt.Sprint(contentLength)
 	if err := client.UpdateAssetFile(ctx, assetFile); err != nil {
-		return nil, errors.Wrap(err, "update asset file failed")
+		return nil, errors.Wrap(err, "failed to update asset file")
 	}
 
 	return asset, nil
